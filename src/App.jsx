3522150fcce632ff6e0cfbbdb822165c00710ebe
Carlo Bruno <carlofbruno@gmail.com>
2019-03-28 15:34:29 -0700

    created card

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import { productData } from './Data/Products.js';
+import Card from './Components/Card';
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: productData,
+      money: 50000
+    };
+  }
+
   render() {
+    let cards = this.state.products.map((product, i) => {
+      return <Card key={i} product={product} />;
+    });
+
     return (
       <div className='App'>
         <header>
@@ -10,13 +25,7 @@ class App extends Component {
           <input type='text' placeholder='search' />
         </header>
 
-        <main className='Content'>
-          {
-            // Card
-            // Card
-            // Card
-          }
-        </main>
+        <main className='Content'>{cards}</main>
       </div>
     );
   }
--- src/Data/Products.js
+++ src/Data/Products.js
@@ -1,68 +1,66 @@
 export const productData = [
-   {
-      name: '',
-      price: '',
-      img:
-         '',
-      category: "",
-      purchaseBefore:false,
-      wishList:false
-   },{
-      name: '',
-      price: '',
-      img:
-         '',
-      category: "",
-      purchaseBefore:false,
-      wishList:false
-   },{
-      name: '',
-      price:  '',
-      img:
-         '',
-      category: "",
-      purchaseBefore:false,
-      wishList:false
-   },{
-      name: '',
-      price: '',
-      img:
-         '',
-      category: "",
-      purchaseBefore:false,
-      wishList:false
-   },{
-      name: '',
-      price: '',
-      img:
-         '',
-      category: "",
-      purchaseBefore:false,
-      wishList:false
-   },{
-      name: '',
-      price: '',
-      img:
-         '',
-      category: "",
-      purchaseBefore:false,
-      wishList:false
-   },{
-      name: '',
-      price: '',
-      img:
-         '',
-      category: "",
-      purchaseBefore:false,
-      wishList:false
-   },{
-      name: '',
-      price: '',
-      img:
-         '',
-      category: "",
-      purchaseBefore:false,
-      wishList:false
-   }
-];  
-
+  {
+    name: 'Arm',
+    price: '2000',
+    img: '',
+    category: 'parts',
+    purchaseBefore: false,
+    wishList: false
+  },
+  {
+    name: 'Leg',
+    price: '3000',
+    img: '',
+    category: 'parts',
+    purchaseBefore: false,
+    wishList: false
+  },
+  {
+    name: 'Shoulder',
+    price: '1500',
+    img: '',
+    category: 'parts',
+    purchaseBefore: false,
+    wishList: false
+  },
+  {
+    name: 'Large Battery',
+    price: '3000',
+    img: '',
+    category: 'battery',
+    purchaseBefore: false,
+    wishList: false
+  },
+  {
+    name: 'Small Battery',
+    price: '2000',
+    img: '',
+    category: 'battery',
+    purchaseBefore: false,
+    wishList: false
+  },
+  {
+    name: 'Kit 1',
+    price: '1000',
+    img: '',
+    category: 'kit',
+    purchaseBefore: false,
+    wishList: false
+  },
+  {
+    name: 'Kit 2',
+    price: '2000',
+    img: '',
+    category: 'kit',
+    purchaseBefore: false,
+    wishList: false
+  },
+  {
+    name: 'Kit 3',
+    price: '3000',
+    img: '',
+    category: 'kit',
+    purchaseBefore: false,
+    wishList: false
+  }
+];
