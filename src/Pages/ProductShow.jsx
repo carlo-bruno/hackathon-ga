30e58f04410c0b222590fac9ef7003c83fc1787e
Carlo Bruno <carlofbruno@gmail.com>
2019-03-29 13:07:49 -0700

    changes in ProductShow

--- src/Pages/ProductShow.jsx
+++ src/Pages/ProductShow.jsx
@@ -5,22 +5,32 @@ import pylon3 from '../images/3-Pylon.png';
 import pylon4 from '../images/4-Pylon.png';
 
 const ProductShow = (props) => {
-  let { name, price, img, rating } = props.product;
+  let { name, price, img, rating, description } = props.product;
 
-  // let pylons;
+  let pylons;
 
-  // switch (rating) {
-  //   case 1:
-  //     pylons = <img src={pylon1} alt='one pylon' />
-  //     break;
-  //   default:
-  //     return null;
-  // }
+  switch (rating) {
+    case 1:
+      pylons = <img src={pylon1} alt='one pylon' />
+      break;
+    case 2:
+      pylons = <img src={pylon2} alt='one pylon' />
+      break;
+    case 3:
+      pylons = <img src={pylon3} alt='one pylon' />
+      break;
+    case 4:
+      pylons = <img src={pylon4} alt='one pylon' />
+      break;
+    default:
+      pylons = <img src={pylon4} alt='one pylon' />
+  }
 
   return (
     <div className="ProductShow Modal" >
 
       <div className="outer" onClick={(e) => props.handleCloseModal(e)}></div>
+
 
       <section className="inner">
         <div className="image-box">
@@ -30,11 +40,16 @@ const ProductShow = (props) => {
         <div className="info-box">
           <h2>{name}</h2>
           <h2>$ {price}</h2>
-          {/* <p>Rating: {pylons}</p> */}
-          <p>Rating: 5</p>
-          <button className="purchase-btn"
-            onClick={(e) => props.handlePurchaseButton(e)}
-          >Purchase</button>
+          <div className="pylon">Rating: {pylons}</div>
+          <h4 className="description">{description}</h4>
+          <div className="button-group">
+            <button className="purchase-btn"
+              onClick={(e) => props.handlePurchaseButton(e)}
+            >Purchase</button>
+            <button className="purchase-btn">
+              Add to Cart
+          </button>
+          </div>
         </div>
       </section>
 
